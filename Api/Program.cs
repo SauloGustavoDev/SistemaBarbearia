9c00ae3db3c9e9911c0b0efca4373214096fa720
Saulo Gustavo <saulo0100@outlook.com>
2025-09-16 22:13:48 -0300

    ajuste para integração com whatsapp

--- Api/Program.cs
+++ Api/Program.cs
@@ -11,6 +11,7 @@ using System.Text;
 
 var builder = WebApplication.CreateBuilder(args);
 
+
 // Add services to the container.
 builder.Services.AddDbContext<Contexto>(options =>
     options.UseNpgsql(
